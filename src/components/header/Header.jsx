4a54746cdc3d1cbb04fcd6a3dd67e283e03acde3
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2025-02-28 11:22:22 +0100

    feat: implement image carousel in Header and update HomeHero component styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import styles from "./header.module.css";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+
+import tentImage from "../../assets/images/tent.jpg";
+import stayImage from "../../assets/images/stay.jpg";
+import tentLightImage from "../../assets/images/tentlights.jpg";
+import weekednImage from "../../assets/images/weekend.jpg";
+
 const Header = ({ header, text, showImage }) => {
+  const images = [tentImage, stayImage, tentLightImage, weekednImage];
   return (
     <div className={styles.container}>
       <h1>{header}</h1>
@@ -9,6 +20,19 @@ const Header = ({ header, text, showImage }) => {
 
       {showImage && (
         <>
+          <Swiper
+            modules={[Autoplay]}
+            slidesPerView={1}
+            autoplay={{ delay: 1000, disableOnInteraction: false }}
+            loop={true}
+          >
+            {images.map((image, index) => (
+              <SwiperSlide key={index}>
+                <img className={styles.slide} src={image} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+
           <img
             className={styles.gittePic}
             src="src/assets/images/gitte.jpg"
--- src/components/hero/HomeHero.jsx
+++ src/components/hero/HomeHero.jsx
@@ -1,36 +1,29 @@
 import React from "react";
 import Navigation from "../navigation/Navigation";
 import styles from "./hero.module.css";
+import tentImage from "../../assets/images/tent.jpg";
+import logoImage from "../../assets/images/logo.png";
 
 function HomeHero() {
   return (
-    <>
-      <div
-        className={styles.hero}
-        style={{ backgroundImage: `url(src/assets/images/tent.jpg)` }}
-      >
-        <div className={styles.navigationWrapper}>
-          <Navigation />
-        </div>
-
-        <div className={styles.overlay}>
-          <div className={styles.content}>
-            <img
-              src="src/assets/images/logo.png"
-              alt="Logo"
-              className={styles.logo}
-            />
-
-            <h1 className={styles.title}>
-              Gittes <br />
-              <span>Glamping</span>
-            </h1>
-
-            <button className={styles.bookButton}>BOOK NOW</button>
-          </div>
+    <div
+      className={styles.hero}
+      style={{ backgroundImage: `url(${tentImage})` }}
+    >
+      <div className={styles.navigationWrapper}>
+        <Navigation />
+      </div>
+      <div className={styles.overlay}>
+        <div className={styles.content}>
+          <img src={logoImage} alt="Logo" className={styles.logo} />
+          <h1 className={styles.title}>
+            Gittes <br />
+            <span>Glamping</span>
+          </h1>
+          <button className={styles.bookButton}>BOOK NOW</button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
